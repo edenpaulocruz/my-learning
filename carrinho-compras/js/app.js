bf5ad3ffc2d4d27c0260855af40db5b5e7292dde
edenpaulocruz <edenpaulo@gmail.com>
2024-03-27 12:17:36 -0300

    Alura Carrinho de Compras - Adição de produtos no carrinho a partir de array

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -9,21 +9,12 @@ let carrinho = [
 carrinho.forEach( item => {
   let nome = item.split(' - ')[0]
   let preco = parseInt(item.split(' - ')[1].replace('R$', ''))
-  console.log(nome, preco);
+  let section = document.createElement('section');
+
+  section.classList.add('carrinho__produtos__produto');
+  section.innerHTML = `
+    <span class="texto-azul">1x</span> ${nome} <span class="texto-azul">R$${preco}</span>
+  `;
+
+  listaProdutos.appendChild(section);
 })
-
-const section = document.createElement('section');
-const section2 = document.createElement('section');
-
-section.classList.add('carrinho__produtos__produto');
-section.innerHTML = `
-  <span class="texto-azul">1x</span> Oculos VR <span class="texto-azul">R$5000</span>
-`;
-
-section2.classList.add('carrinho__produtos__produto');
-section2.innerHTML = `
-<span class="texto-azul">1x</span> Fone de ouvido <span class="texto-azul">R$100</span>
-`;
-
-listaProdutos.appendChild(section);
-listaProdutos.appendChild(section2);
